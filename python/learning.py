99985603a285a055d30aca18143e4d0782ca488d
babal1111 <babaldon1111@gmail.com>
2025-08-11 13:58:23 +0530

    revised type conversion

---
+++ python/learning.py
@@ -0,0 +1,21 @@
+
+print("hrllo ")
+
+name = "babal"
+print(name) 
+
+# name = input("what's ur name : ")
+# print(name) 
+#type conve
+old_age = input("enter ur old age : ")
+new_age = int(old_age)+10
+print(new_age)
+
+num1 = input("num1: ")
+num2 = input("num2: ")
+sum = num1+ num2
+print(sum)
+
+sum = int(num1)+ int(num2)
+print(sum)
+ 
